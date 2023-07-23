b572b4e8e6a799b7342e65f15406c174f88e5d7f
obelisk477 <cwbatte2@gmail.com>
2023-07-22 22:10:37 -0400

    Added route & db config boilerplate

---
+++ routes/api/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const thoughtRoutes = require('./thoughtRoutes')
+const userRoutes = require('./userRoutes')
+
+router.use('/users', userRoutes)
+router.use('/thoughts', thoughtRoutes)
